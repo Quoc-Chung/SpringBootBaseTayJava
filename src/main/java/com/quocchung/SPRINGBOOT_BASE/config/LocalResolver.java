5a989abdf25f1a3e91a0c7fc5d9fe546bbfab03a
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-05-08 23:53:35 +0700

    da ngon ngu tam thoi

--- src/main/java/com/quocchung/SPRINGBOOT_BASE/config/LocalResolver.java
+++ src/main/java/com/quocchung/SPRINGBOOT_BASE/config/LocalResolver.java
@@ -1,8 +1,21 @@
 package com.quocchung.SPRINGBOOT_BASE.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Locale;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
-//public class LocalResolver extends AcceptHeaderLocaleResolver implements LocaleChangeInterceptor {
-//
-//}
+
+public class LocalResolver extends AcceptHeaderLocaleResolver implements WebMvcConfigurer {
+
+  @Override
+  public Locale resolveLocale(HttpServletRequest request) {
+    String language_header = request.getHeader("Accept-Language");
+
+    return StringUtils.hasLength(language_header) ? Locale.lookup(Locale.LanguageRange.parse(language_header),
+        List.of(new Locale("en"), new Locale("fr"))
+    ) : Locale.getDefault();
+  }
+}
